9c739638387078fb86ffb83d363365998afcd5f9
Gabriel Andrade <gabrielgustavoandrade@gmail.com>
2021-08-09 10:44:17 -0300

    fixind the build

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from 'express'
+import express from "express";
 import { insertExcel } from "./utils/insertData";
-import pokemonRoutes from './routes/pokemon.routes'
+import pokemonRoutes from "./routes/pokemon.routes";
 
 const app: express.Application = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // app.use(helmet())
 
-app.use('/pokemon', pokemonRoutes)
+app.use("/pokemon", pokemonRoutes);
 
 app.listen(8080, async () => {
   console.log(`servidor rodando em http://localhost:8080`);
