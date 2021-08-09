2bedc888c5dfb4eae5dee48ed985f7b4cdbcfb49
Gabriel Andrade <gabrielgustavoandrade@gmail.com>
2021-08-09 12:26:46 -0300

    setting cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 import { insertExcel } from "./utils/insertData";
 import pokemonRoutes from "./routes/pokemon.routes";
 
@@ -6,7 +7,7 @@ const app: express.Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(helmet())
+app.use(cors())
 
 app.use("/pokemon", pokemonRoutes);
 
---
+++ src/utils/serverData.ts
@@ -0,0 +1,1 @@
+export const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:3000'
